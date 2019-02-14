1000fd34940fdb0f02a82f6d1ddb64f664aff8ba
Artsiom Sinitski <artsiom.vs@gmail.com>
2019-02-14 21:24:59 +0300

    added a getter for the storage data

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,7 +1,11 @@
 pragma solidity ^0.5.0;
 
 contract SimpleStorage {
-    string public storedData;
+    string private storedData;
+
+    function getStoredData() public view returns (string memory) {
+        return storedData;
+    }
 
     function setStoredData(string memory _data) public {
         storedData = _data;
