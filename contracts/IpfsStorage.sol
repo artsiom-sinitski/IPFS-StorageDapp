0010c5ee4ad94881d0fd7303e4798c09dec31fce
Artsiom Sinitski <artsiom.vs@gmail.com>
2019-02-19 22:33:30 +0300

    completed storage contract functionality and added unit tests

--- contracts/IpfsStorage.sol
+++ contracts/IpfsStorage.sol
@@ -15,67 +15,60 @@ contract IpfsStorage {
     // maps IPFS object's address to its meta data
     mapping(string => IPFSmetaData) private storedData;
 
-
     //*******EVENTS and MODIFIERS ************
     //****************************************
-    event MediaStoredOnIPFS(string ipfsAddress, uint32 keyIndex);
+    event MediaStoredOnIPFS(string ipfsAddress, string mediaType, string description);
 
-    modifier beValid(string memory _ipfsAddress) {
-        //string to bytes conversion for 'free' functionality
-        bytes memory s = bytes(_ipfsAddress);
-        uint32 ipfsHashSize = uint32(s.length);
-        // length of IPFS hash should be 46 bytes
-        require(ipfsHashSize == 46);
-        _;
-    }
-
-    modifier beExisting(string memory _ipfsAddress) {
-        require(keyExists(_ipfsAddress) == true, "IPFS address not found!");
-        _;
-    }
+    // modifier beExisting(string memory _ipfsAddress) {
+    //     require(keyExists(_ipfsAddress) == true, "IPFS address not found!");
+    //     _;
+    // }
     //****** Contract Public Interface ******
     //***************************************
     constructor() public {}
 
     //TO DO: add the 'type' field
-    function storeMediaToIPFS(string memory _ipfsAddress, 
-                           string memory _description)
-      public beValid(_ipfsAddress)
-     returns (uint32 keyIndex) {
-        require(keyExists(_ipfsAddress) == false, "duplicate IPFS address!");
-        //store the IPFS object in the mapping
-        storedData[_ipfsAddress].mediaType = "image";
-        storedData[_ipfsAddress].description = _description;
-        //the '.push' method will return the new length of array
-        storedData[_ipfsAddress].index = uint32(storedDataIndexes.push(_ipfsAddress) - 1);
-        emit MediaStoredOnIPFS(_ipfsAddress, keyIndex);
-        return keyIndex;
+    function storeMediaToIPFS(string memory _ipfsAddress,
+                              string memory _mediaType,
+                              string memory _description)
+      public returns (bool success) {
+        //require(keyExists(_ipfsAddress) == false, "duplicate IPFS address!");
+        if (!keyExists(_ipfsAddress)) {
+            //store the IPFS object in the mapping
+            storedData[_ipfsAddress].mediaType = _mediaType;
+            storedData[_ipfsAddress].description = _description;
+            //the '.push' method will return the new length of array
+            storedData[_ipfsAddress].index = uint32(storedDataIndexes.push(_ipfsAddress) - 1);
+            emit MediaStoredOnIPFS(_ipfsAddress, _mediaType, _description);
+            return true;
+        }
+        return false;
     }
 
     function getStoredDataType(string memory _ipfsAddress)
-      public view beValid(_ipfsAddress)
-                  beExisting(_ipfsAddress)
-     returns (string memory dataType) {
+      public view returns (string memory dataType) {
         return storedData[_ipfsAddress].mediaType;
     }
 
     function getStoredDataDescription(string memory _ipfsAddress)
-      public view beValid(_ipfsAddress)
-                  beExisting(_ipfsAddress)
-     returns (string memory dataDescription) {
+      public view returns (string memory dataDescription) {
         return storedData[_ipfsAddress].description;
     }
 
     function getStoredDataIndex(string memory _ipfsAddress)
-      public view beValid(_ipfsAddress)
-                  beExisting(_ipfsAddress)
-     returns (uint32 dataIndex) {
+      public view returns (uint32 dataIndex) {
         return storedData[_ipfsAddress].index;
     }
 
+    function getStoredDataSize() 
+      public view returns (uint32 len) {
+          return uint32(storedDataIndexes.length);
+      }
+
     //****** Contract Public Interface ******
     //***************************************
-    function keyExists(string memory _key) private view returns (bool keyFound) {
+    function keyExists(string memory _key)
+     private view returns (bool keyFound) {
         if (storedDataIndexes.length <= 0 ) {
             return false;
         }
@@ -83,4 +76,5 @@ contract IpfsStorage {
         bytes memory _bk = bytes(_key);
         return (keccak256(_bk) == keccak256(bk1));
     }
+
 }
--- test/TestIpfsStorage.js
+++ test/TestIpfsStorage.js
@@ -1,11 +1,11 @@
 var IpfsStorage = artifacts.require("./IpfsStorage.sol");
 
 contract('IpfsStorage', function(accounts) {
-    let invalidIPFSaddress = "Qmb75_57VmV";
-    let ipfsAddress = "QmWmyoMoctfbAaiEs2G46gpeUmhqFRDW6KWo64y5r581Vz";
+    let ipfsAddress1 = "QmWmyoMoctfbAaiEs2G46gpeUmhqFRDW6KWo64y5r581Vz";
+    let ipfsAddress2 = "QmWmyoMoctfbAaiEs2GFLRHEUIPH8afierertdfHRE1243";
 
     let mediaType1 = "image";
-    //let mediaType2 = "video";
+    let mediaType2 = "video";
 
     let desc1 = "Test description";
     let desc2 = "Description2";
@@ -13,29 +13,43 @@ contract('IpfsStorage', function(accounts) {
     it('tests the basic functionality of the storage contract', function() {
         return IpfsStorage.deployed().then(function(instance) {
             storageInstance = instance;
-            return storageInstance.storeMediaToIPFS(ipfsAddress, desc1);
-        /*}).then(assert.fail).catch(function(error) {
-            console.log("Error: ", error);
-            assert.equal(error.message.indexOf('Error') >= 0, 'IPFS address must be 46 characters long!');
-            storageInstance.storeMediaToIPFS(ipfsAddress, desc1);
-        /*}).then(assert.fail).catch(function(error) {
-            console.log("Error: ", error);
-            assert.equal(error.message.indexOf('revert') >= 0, 'IPFS address already exists!'); */
+            return storageInstance.storeMediaToIPFS(ipfsAddress1, mediaType1, desc1);
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1, 'triggers exactly one event');
+            assert.equal(receipt.logs[0].event, 'MediaStoredOnIPFS', 'should be the "MediaStoredOnIPFS" event');
+            assert.equal(receipt.logs[0].args.ipfsAddress, ipfsAddress1, 'logs the IPFS hash');
+            return storageInstance.getStoredDataSize();
+        }).then(function(len) {
+            assert.equal(len, 1, 'should be stored only a single IPFS hash')
+            return storageInstance.getStoredDataType(ipfsAddress1);
         }).then(function(result) {
-            //console.log("Result:", result);
-            console.log("Reslt->Logs->args ", result.logs[0].args);
-            //console.log("Reslt->logs[0]->args[0]: ", result.logs[0].args.keyIndex);
-            let arrSize = (result.logs[0].args.keyIndex).toNumber() + 1;
-            assert.equal(arrSize, 1, 'indexes array has the correct size');
-            return storageInstance.getStoredDataType(ipfsAddress);
+            assert.equal(result.toString(), mediaType1, 'has the correct media type 1');
+            return storageInstance.getStoredDataDescription(ipfsAddress1);
         }).then(function(result) {
-            //console.log("dataType: ", result);
-            assert.equal(result.toString(), mediaType1, 'has the correct media type');
-            return storageInstance.getStoredDataDescription(ipfsAddress);
+            assert.equal(result.toString(), desc1, 'has the correct media description 1');
+            return storageInstance.storeMediaToIPFS(ipfsAddress1, mediaType1, desc1);
+        }).then(function(receipt) {
+            //console.log("store again Result: ", receipt);
+            assert.equal(receipt.logs.length, 0, 'no "MediaStoredOnIPFS" event trigerred');
+            return storageInstance.getStoredDataSize();
+        }).then(function(len) {
+            assert.equal(len.toNumber(), 1, 'should still be stored only a single IPFS hash')
+            return storageInstance.storeMediaToIPFS(ipfsAddress2, mediaType2, desc2);
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1, 'triggers exactly one event');
+            assert.equal(receipt.logs[0].event, 'MediaStoredOnIPFS', 'should be the "MediaStoredOnIPFS" event 2');
+            assert.equal(receipt.logs[0].args.ipfsAddress, ipfsAddress2, 'logs the IPFS hash');
+            return storageInstance.getStoredDataSize();
+        }).then(function(len) {
+            assert.equal(len.toNumber(), 2, 'should now be stored 2 IPFS hashes');
+            return storageInstance.getStoredDataType(ipfsAddress2);
         }).then(function(result) {
-            //console.log("Description: ", result);
-            assert.equal(result.toString(), desc1, 'has the correct media description');
+            assert.equal(result.toString(), mediaType2, 'has the correct media type 2');
+            return storageInstance.getStoredDataDescription(ipfsAddress2);
+        }).then(function(result) {
+            assert.equal(result.toString(), desc2, 'has the correct media description 2');
         })
+        
     });
 
 });
